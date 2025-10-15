391267c21335d5052ecf372fda2f0111581db432
Jan Stasieńko <stas.szymonienko@gmail.com>
2025-10-15 15:37:36 +0200

    Comment test

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld
     {
         System.out.println("Hello World");
         System.out.println("Test na githubie");
+        //Patrz Wojtek pushuje Valorant jest super cho grać w czołgi
     }
 }
